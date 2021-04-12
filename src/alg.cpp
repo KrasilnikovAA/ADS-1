c4c97f3b1177654cda1d8f959d71f66cacae7c21
KrasilnikovAA <79089177+KrasilnikovAA@users.noreply.github.com>
2021-04-12 20:57:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+ int k=0;
+cin>>size;
+for (int i=0;i<size;i++)
+  cin>>arr[i];
+cin>>value;
+  for (int i=0;i<size;i++)
+    if (arr[i]=value)
+      k++;
+  if (k>0)
+    return k;
+  else
   return 0; // если ничего не найдено
 }
