dc17b7f5f9bf67fbe1c983fe3fb75f5858636e59
KrasilnikovAA <79089177+KrasilnikovAA@users.noreply.github.com>
2021-04-12 20:44:22 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -3,5 +3,15 @@
 #define INCLUDE_ALG_H_
 
 int cbinsearch(int *arr, int size, int value);
-
+int kol=0;
+cin>>size;
+for (int i=0;i<size;i++)
+  cin>>arr[i];
+cin>>value;
+for(int i=0;i<size;i++)
+{
+  if (arr[i]=value)
+    kol++;
+}
+return kol;
 #endif  // INCLUDE_ALG_H_
